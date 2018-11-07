f59dd0adc1faa87e2f3011e2870f82ddd182298f
Arne Van de Bemdt <a.vandebemdt@icloud.com>
2018-11-07 14:30:14 +0100

    refactoring structure

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,7 @@
-package app;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controllers"})
+@SpringBootApplication(scanBasePackages = {"controller"})
 public class Application {
 
     public static void main(String[] args) {
